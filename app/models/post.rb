525a3f7f3b0fb0353551655335f859fdbc8ce35b
Alessandra Vertrees <avertrees447@gmail.com>
2019-08-14 09:20:10 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ActiveRecord::Base
     validates :content, length:{minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validates :is_clickbait
+
+  def :is_clickbait
+    clickbait_words = ["Won't Believe", "Secret", "Top
+[number]", "Guess"]
+    if !clickbait_words.include?(self.summary)
+      return false
+    end
+  end
 end
